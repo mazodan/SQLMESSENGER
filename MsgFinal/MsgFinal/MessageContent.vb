df357babf28bf4b1e40a2598783724c05b5b7ddf
mazodan <danmazo32@gmail.com>
2016-05-09 19:32:20 +0800

    Inbox Complete - Outbox only left

--- MsgFinal/MsgFinal/MessageContent.vb
+++ MsgFinal/MsgFinal/MessageContent.vb
@@ -1,7 +1,7 @@
 ﻿Public Class MessageContent
 
     Private Sub btnReply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReply.Click
-        Compose.rtfMessage.Clear()
+        Compose.rtfMessage.Text = ""
         Compose.cmbFriends.Items.Clear()
         Compose.cmbFriends.Items.Add(lblSend.Text)
         Compose.cmbFriends.Text = lblSend.Text
@@ -11,5 +11,16 @@
 
     Private Sub MessageContent_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
         MessageList.Show()
+    End Sub
+
+    Private Sub btnForward_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnForward.Click
+        Compose.rtfMessage.Text = ""
+        Compose.cmbFriends.Items.Clear()
+        For Each elm As String In getFriendsToList(getUsrID(CurUser))
+            Compose.cmbFriends.Items.Add(elm)
+        Next
+        Compose.rtfMessage.Rtf = rtfInfo.Rtf
+        Compose.txtSubject.Text = "FW: " + lblSubj.Text
+        Compose.ShowDialog()
     End Sub
 End Class
